bbaa39b3268be837ebe4d8c3aee131cf9217ee0e
mmartinez@infuy.com <mmartinez@infuy.com>
2019-08-20 16:24:14 -0300

    Code cleanup

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ new CronJob('*/10 * * * *', ()=>{
 getLogs =  () =>{
     request.post('http://localhost:4444', {
         json: {"jsonrpc":"2.0","method":"eth_getLogs","params":[{  "address":"0xde2D53e8d0E673A4b1D9054cE83091777F2fd8Ce", "fromBlock": "0x0", "toBlock": "latest" }],"id":74}
-
     }, (error, res, body) => {
         if (error) {
-            console.error(error)
-            return
+            console.error(error);
         }
         console.log(`statusCode: ${res.statusCode}`);
-        console.log(res.body.result)
+        console.log(res.body.result);
     })
 }
